9d9849b246a100ac1974743bc57d9a4262513007
ValdirCezar <valdircezar312@gmail.com>
2021-01-10 17:49:29 -0300

    excecao personalizada para objeto nao encontrado

--- src/main/java/com/valdir/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/valdir/bookstore/resources/CategoriaResource.java
@@ -23,5 +23,3 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 	}
 }
-
-// localhost:8080/categorias/1
--- src/main/java/com/valdir/bookstore/service/CategoriaService.java
+++ src/main/java/com/valdir/bookstore/service/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.valdir.bookstore.domain.Categoria;
 import com.valdir.bookstore.repositories.CategoriaRepository;
+import com.valdir.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
