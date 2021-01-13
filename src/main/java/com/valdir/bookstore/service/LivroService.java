3a45d525c4ac4d17e7e43e48766f240fca994c7d
ValdirCezar <valdircezar312@gmail.com>
2021-01-13 08:52:38 -0300

    PUT e PATCH para Livro

--- src/main/java/com/valdir/bookstore/service/LivroService.java
+++ src/main/java/com/valdir/bookstore/service/LivroService.java
@@ -15,7 +15,7 @@ public class LivroService {
 
 	@Autowired
 	private LivroRepository repository;
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
 
@@ -28,6 +28,18 @@ public class LivroService {
 	public List<Livro> findAll(Integer id_cat) {
 		categoriaService.findById(id_cat);
 		return repository.findAllByCategoria(id_cat);
+	}
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
 	}
 
 }
